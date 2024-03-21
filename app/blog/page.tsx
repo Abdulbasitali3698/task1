4219a9877decf24bb15fa26723c39132fca53b34
Abdulbasitali3698 <43988292+Abdulbasitali3698@users.noreply.github.com>
2024-03-21 23:12:33 +0500

    .

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,50 @@
+"use client"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-import Link from 'next/link'
-import React from 'react'
-
-export default function Blog() {
+function MyVerticallyCenteredModal(props) {
   return (
-    <div className='ml-4'>
-        <h1 className=' text-xl font-bold'>
-            Blog Page
-        </h1>
-        <h3>
-          <Link href="/blog/[blogs]?blogs=1">go to the Blogs</Link>
-        </h3>
-        
-    </div>
-  )
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default function App() {
+  const [modalShow, setModalShow] = useState(false);
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
 }
